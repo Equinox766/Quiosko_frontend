daea6129b86244999e90f3ac789695e9c32efe0a
Equinox766 <equinox766@gmail.com>
2023-06-14 11:16:49 -0400

    Completando el sidebar para para las categorias

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,10 +1,17 @@
 import { Outlet } from 'react-router-dom'
+import Resumen from '../components/Resumen'
+import Sidebar from '../components/Sidebar'
 
 export default function layout() {
   return (
-    <div>
-      layout
-      <Outlet />
+    <div className='md:flex'>
+      <Sidebar />
+
+      <main className='flex-1'>
+        <Outlet />
+      </main>
+      
+      <Resumen />
     </div>
   )
 }
